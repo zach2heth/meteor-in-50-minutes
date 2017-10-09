06c8a93c010d1d14be547cc48f2c160d748b71df
zach2heth <zach2heth@gmail.com>
2017-10-08 14:18:24 -1000

    Task 3: Tasklist form

--- app/tasklist.js
+++ app/tasklist.js
@@ -6,6 +6,28 @@ if(Meteor.isClient){
       return Tasks.find({}, {sort: {createdAt: -1}});
     }
   })
+
+  Template.tasks.events({
+
+    "submit .add-task":function (event){
+      var name = event.target.name.value;
+
+      Tasks.insert({
+        name: name,
+        createAt: new Date()
+      });
+      event.target.name.value ='';
+      return false;
+    },
+
+    "click .delete-task": function (event){
+      if(confirm('Delete Task?')){
+        Tasks.remove(this._id);
+      }
+      return false;
+    }
+
+  })
 }
 
 if(Meteor.isServer){
