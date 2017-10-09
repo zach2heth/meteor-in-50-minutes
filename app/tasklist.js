72141602c0b0056d1a497b63a21d71fd3967f17c
zach2heth <zach2heth@gmail.com>
2017-10-08 15:12:21 -1000

    Task 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,28 +1,26 @@
 Tasks = new Mongo.Collection('tasks');
 
-if(Meteor.isClient){
+if (Meteor.isClient) {
   Template.tasks.helpers({
-    tasks: function(){
-      return Tasks.find({}, {sort: {createdAt: -1}});
+    tasks: function () {
+      return Tasks.find({}, { sort: { createdAt: -1 } });
     }
   })
 
   Template.tasks.events({
 
-    "submit .add-task":function (event){
+    "submit .add-task": function (event) {
       var name = event.target.name.value;
 
-      Tasks.insert({
-        name: name,
-        createAt: new Date()
-      });
-      event.target.name.value ='';
+      Meteor.call('addTask', name);
+
+      event.target.name.value = '';
       return false;
     },
 
-    "click .delete-task": function (event){
-      if(confirm('Delete Task?')){
-        Tasks.remove(this._id);
+    "click .delete-task": function (event) {
+      if (confirm('Delete Task?')) {
+        Meteor.call('deleteTask',this._id);
       }
       return false;
     }
@@ -30,6 +28,23 @@ if(Meteor.isClient){
   })
 }
 
-if(Meteor.isServer){
+if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function (name) {
+    if (!Meteor.userId()) {
+      throw new Meteor.error('No access');
+    }
+    Tasks.insert({
+      name: name,
+      createAt: new Date(),
+      userId: Meteor.userId()
+    });
+  },
+
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId)
+  }
+});
